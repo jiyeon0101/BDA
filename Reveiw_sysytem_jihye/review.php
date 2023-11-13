d64da44d58bfe2cc8589f4645a60dfcd9e2e2a6b
parkjihy <74035930+parkjihy@users.noreply.github.com>
2023-11-13 14:24:12 +0900

    Update review.php

--- Reveiw_sysytem_jihye/review.php
+++ Reveiw_sysytem_jihye/review.php
@@ -78,8 +78,9 @@
         </ul>
     </nav>
     <div class="section">
-        <h1>Ranking_review</h1>
-        <span>Overall ranking by stars (0-5), reviews (also for each store)</span>
+        <h1>Ranking store</h1>
+        <span>About Ranking store</span>
+        <div>
         <?php
             $servername = "localhost";
             $username = "root";
@@ -116,6 +117,60 @@
             // 연결 종료
             $conn->close();
         ?>
-    </div>
+        </div>
+        <h1>review_comments</h1>
+        <span>About comments</span>
+        <div>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "restaurant_data";
+
+        // MySQL 연결
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // 연결 확인
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // 각 상점별 평균 랭킹 쿼리
+        $sql = "SELECT store_ID, AVG(ranking) as avg_ranking
+                FROM Reviews
+                GROUP BY store_ID
+                ORDER BY avg_ranking ASC";
+
+        $result = $conn->query($sql);
+
+        // 결과 출력
+        if ($result->num_rows > 0) {
+            echo "<table><tr><th>Store ID</th><th>Average Ranking</th><th>Top 3 Comments</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                $storeID = $row["store_ID"];
+                $avgRanking = $row["avg_ranking"];
+
+                // 각 상점별 상위 3개 코멘트 쿼리
+                $commentSql = "SELECT comments FROM Reviews WHERE store_ID = '$storeID' ORDER BY ranking ASC LIMIT 3";
+                $commentResult = $conn->query($commentSql);
+
+                // 코멘트 출력
+                $comments = [];
+                while ($commentRow = $commentResult->fetch_assoc()) {
+                    $comments[] = $commentRow["comments"];
+                }
+
+                echo "<tr><td>" . $storeID . "</td><td>" . $avgRanking . "</td><td>" . implode(", ", $comments) . "</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No results found.";
+        }
+
+        // 연결 종료
+        $conn->close();
+        ?>
+
+        </div>
 </body>
 </html>
