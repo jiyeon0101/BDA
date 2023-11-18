e80105d7b830b3a685618c84cd5e2f927990a3a5
jiyeon0101 <jyk5350@gmail.com>
2023-11-18 18:25:26 +0900

    staff second

--- Reveiw_system_jihye/addstock.php
+++ Reveiw_system_jihye/addstock.php
@@ -38,7 +38,6 @@ if (isset($_POST['add'])) {
   
     $query = "INSERT INTO inventory (stock_name,stock_amount,cost) 
   			  VALUES('$stock_name','$stock_amount','$stock_cost')";
-          
       if(mysqli_query($db, $query))
       {
       echo "<script>alert('Successfully stored');</script>";
@@ -52,3 +51,36 @@ if (isset($_POST['add'])) {
   
 }
 ?>
+<!--
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Item</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
+<form method="POST" class="form-inline" action="additem.php">
+  <div class="form-group">
+    <label for="name">Product Name</label>
+    <input type="text" class="form-control" name="product_name">
+    
+  </div>
+  <div class="form-group">
+    <label for="name">Price</label>
+    <input type="text" class="form-control" name="price">
+  </div>
+  <div>
+        <label for="name">Quantity</label>
+        <input type="number" name="quant" id="quant" min="1" max="">
+    </div>
+  <button type="submit" class="btn btn-default" name="add">Add item</button>
+</form> 
+
+<div>
+<a href="table.php">Home</a>
+</div>
+</body>
+</html>
+<!-->
